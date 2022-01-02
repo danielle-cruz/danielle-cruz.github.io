c1bdf951d1af0694a411ae611b367e8a11525ac4
Danielle Cruz <dcruz21@stanford.edu>
2022-01-01 20:52:09 -0600

    Added changing colors to all thumbnails

--- index.js
+++ index.js
@@ -1,87 +1,74 @@
-/* const COLORS = [
-    "#52A6D4",
-    "#FF0404",
-    "#EF9D23",
-    "#45863A",
-    "#0041FF",
-    "#FF0D86",
-    "#500484",
-];
-
-const COLORS = [
-    {
-        name: "index",
-        color: "#52A6D4"
-    },
-    {
-        name: "altogether",
-        color: "#FF0404"
-    },
-    {
-        name: "ted-lasso",
-        color: "#EF9D23"
-    },
-    {
-        name: "guia",
-        color: "#0041FF"
-    },
-    {
-        name: "soundsight",
-        color: "#45863A"
-    },
-    {
-        name: "buildagram",
-        color: "#500484"
-    },
-    {
-        name: "fil-am",
-        color: "#52A6D4"
-    },
-    {
-        name: "linked-out",
-        color: "#FF0D86"
-    },
-    {
-        name: "in-flux",
-        color: "#EF9D23"
-    },
-
-]
-
-var colorIndex = 0;
-var currentColor = COLORS[colorIndex];
-
-function getRandomColor() {
-    colorIndex = (colorIndex + 1) % COLORS.length;
-    return COLORS[colorIndex];
-}
-
-var altogetherThumbnail = document.querySelector("#altogether");
-var colorElements = document.querySelectorAll("h1, .block-quote, .external-link");
-
-altogetherThumbnail.addEventListener("mouseover", (event) => {
-    for(var i = 0; i < colorElements.length; i++) {
-        colorElements[i].style.color = getRandomColor();
-    }
-}); */
-
 const COLORS = {
     "index": "#52A6D4",
     "altogether": "#FF0404",
-    "ted-lasso": "#EF9D23",
+    "tedLasso": "#EF9D23",
     "guia": "#0041FF",
     "soundsight": "#45863A",
     "buildagram": "#500484",
-    "fil-am": "#52A6D4",
-    "linked-out": "#FF0D86",
-    "in-flux": "#EF9D23"
+    "filAm": "#52A6D4",
+    "linkedOut": "#FF0D86",
+    "inFlux": "#EF9D23"
 }
 
+/* Get elements that should be re-colored */
 var colorElements = document.querySelectorAll("h1, .block-quote, .external-link");
-var altogetherThumbnail = document.querySelector("#altogether");
-altogetherThumbnail.addEventListener("mouseover", (event) => {
-    currentColor = COLORS.altogether;
+var altogetherTitle = document.querySelector("#altogether > h2")
+
+
+/* Get all thumbnails */
+var altogether = document.querySelector("#altogether");
+var tedLasso = document.querySelector("#ted-lasso");
+var guia = document.querySelector("#guia");
+var soundsight = document.querySelector("#soundsight");
+var buildagram = document.querySelector("#buildagram");
+var filAm = document.querySelector("#fil-am");
+var linkedOut = document.querySelector("#linked-out");
+var inFlux = document.querySelector("#in-flux");
+
+/* ALTogether */
+altogether.addEventListener("mouseover", (event) => {
     for(var i = 0; i < colorElements.length; i++) {
         colorElements[i].style.color = COLORS.altogether;
+    }
+});
+
+tedLasso.addEventListener("mouseover", (event) => {
+    for(var i = 0; i < colorElements.length; i++) {
+        colorElements[i].style.color = COLORS.tedLasso;
+    }
+});
+
+guia.addEventListener("mouseover", (event) => {
+    for(var i = 0; i < colorElements.length; i++) {
+        colorElements[i].style.color = COLORS.guia;
+    }
+});
+
+soundsight.addEventListener("mouseover", (event) => {
+    for(var i = 0; i < colorElements.length; i++) {
+        colorElements[i].style.color = COLORS.soundsight;
+    }
+});
+
+buildagram.addEventListener("mouseover", (event) => {
+    for(var i = 0; i < colorElements.length; i++) {
+        colorElements[i].style.color = COLORS.buildagram;
+    }
+});
+
+filAm.addEventListener("mouseover", (event) => {
+    for(var i = 0; i < colorElements.length; i++) {
+        colorElements[i].style.color = COLORS.filAm;
+    }
+});
+
+linkedOut.addEventListener("mouseover", (event) => {
+    for(var i = 0; i < colorElements.length; i++) {
+        colorElements[i].style.color = COLORS.linkedOut;
+    }
+});
+inFlux.addEventListener("mouseover", (event) => {
+    for(var i = 0; i < colorElements.length; i++) {
+        colorElements[i].style.color = COLORS.inFlux;
     }
 });
