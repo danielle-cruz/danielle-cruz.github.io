0475888ee2fe24021c6c49fc341df76be9e426ba
Danielle Cruz <dcruz21@stanford.edu>
2023-09-05 03:08:40 -0500

    Test build without JavaScript

--- v2/index.js
+++ v2/index.js
@@ -1,84 +1,84 @@
-/* Globals */
-const FILTERS = [
-    "accessibility",
-    "data visualization",
-    "education",
-    "storytelling",
-    "identity",
-    "play",
-    "language",
-    "art",
-    "music"
-]
+// /* Globals */
+// const FILTERS = [
+//     "accessibility",
+//     "data visualization",
+//     "education",
+//     "storytelling",
+//     "identity",
+//     "play",
+//     "language",
+//     "art",
+//     "music"
+// ]
 
-const WORKS = {
-    "barkada": ["storytelling", "identity", "play"],
-    "altogether": ["accessibility"]
-}
+// const WORKS = {
+//     "barkada": ["storytelling", "identity", "play"],
+//     "altogether": ["accessibility"]
+// }
 
-let selectedFilters = [];
-let selectedWorks = new Set();
+// let selectedFilters = [];
+// let selectedWorks = new Set();
 
-/* Elements */
-const filterContainer = document.querySelector("#filter-container");
-const gallery = document.querySelector("#gallery");
-const thumbnails = document.querySelectorAll("#gallery a");
+// /* Elements */
+// const filterContainer = document.querySelector("#filter-container");
+// const gallery = document.querySelector("#gallery");
+// const thumbnails = document.querySelectorAll("#gallery a");
 
-/* Create filter buttons */
-for (let i = 0; i < FILTERS.length; i++) {
-    const filter = document.createElement("button");
-    filter.textContent = FILTERS[i];
-    filter.classList.add("filter-button");
-    filter.addEventListener("click", (event) => {onSelectFilter(filter);});
-    filterContainer.append(filter);
-}
+// /* Create filter buttons */
+// for (let i = 0; i < FILTERS.length; i++) {
+//     const filter = document.createElement("button");
+//     filter.textContent = FILTERS[i];
+//     filter.classList.add("filter-button");
+//     filter.addEventListener("click", (event) => {onSelectFilter(filter);});
+//     filterContainer.append(filter);
+// }
 
-/* Event listeners */
+// /* Event listeners */
 
-function onSelectFilter(filter) {
-    console.log(filter.textContent);
-    if (filter.classList.contains("selected")) {
-        filter.classList.remove("selected");
-        const i = selectedFilters.indexOf(filter.textContent);
-        selectedFilters.splice(i, 1);
-    } else {
-        filter.classList.add("selected");
-        selectedFilters.push(filter.textContent);
-    }
-    updateSelectedWorks();
-    displaySelectedWorks();
-}
+// function onSelectFilter(filter) {
+//     console.log(filter.textContent);
+//     if (filter.classList.contains("selected")) {
+//         filter.classList.remove("selected");
+//         const i = selectedFilters.indexOf(filter.textContent);
+//         selectedFilters.splice(i, 1);
+//     } else {
+//         filter.classList.add("selected");
+//         selectedFilters.push(filter.textContent);
+//     }
+//     updateSelectedWorks();
+//     displaySelectedWorks();
+// }
 
-/* Helper functions */
+// /* Helper functions */
 
-function updateSelectedWorks() {
-    for (const work in WORKS) {
-        if (shouldSelectWork(work) == true) {
-            selectedWorks.add(work);
-        } else {
-            if (selectedWorks.has(work)) {
-                selectedWorks.delete(work);
-            }
-        }
-    }
-}
+// function updateSelectedWorks() {
+//     for (const work in WORKS) {
+//         if (shouldSelectWork(work) == true) {
+//             selectedWorks.add(work);
+//         } else {
+//             if (selectedWorks.has(work)) {
+//                 selectedWorks.delete(work);
+//             }
+//         }
+//     }
+// }
 
-function shouldSelectWork(work) {
-    for (const filter of selectedFilters) {
-        if (!WORKS[work].includes(filter)) {
-            return false;
-        }
-    }
-    return true;
-}
+// function shouldSelectWork(work) {
+//     for (const filter of selectedFilters) {
+//         if (!WORKS[work].includes(filter)) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
 
-function displaySelectedWorks() {
-    for (const thumbnail of thumbnails) {
-        if (selectedWorks.has(thumbnail.id)) {
-            thumbnail.style.display = "inline-block";
-        } else {
-            thumbnail.style.display = "none";
-        }
-    }
-}
+// function displaySelectedWorks() {
+//     for (const thumbnail of thumbnails) {
+//         if (selectedWorks.has(thumbnail.id)) {
+//             thumbnail.style.display = "inline-block";
+//         } else {
+//             thumbnail.style.display = "none";
+//         }
+//     }
+// }
 
